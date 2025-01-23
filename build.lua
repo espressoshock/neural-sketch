69d0d8e0541ecb314a782beb9970df574c1829f7
espressoshock <vincenzo.buono.phd@gmail.com>
2025-01-23 04:17:29 +0100

    feat: basic transition from dev sty->dtx

--- build.lua
+++ build.lua
@@ -1,9 +1,7 @@
 #!/usr/bin/env texlua
 
--- Build script for neural-sketch
-
-bundle = "neural-sketch"
 module = "neural-sketch"
+bundle = "neural-sketch"
 
 uploadconfig = {
 	author = "Vincenzo Buono",
@@ -16,13 +14,12 @@ uploadconfig = {
 	ctanPath = "/graphics/...",
 	repository = "https://github.com/espressoshock/neural-sketch",
 	bugtracker = "https://github.com/espressoshock/neural-sketch/issues",
-	description = [[A LaTeX package that leverages TikZ to create publication-quality figures for machine learning diagrams, system overviews, neural networks, algorithms, and more.]],
+	description = [[A LaTeX package that leverages TikZ to create publication-quality figures for
+                  machine learning diagrams, system overviews, neural networks, algorithms, and more.]],
 	update = true,
 }
 
-typesetfiles = { "doc/latex/neural-sketch/neural-sketch-doc.tex" }
-sourcefiles = { "src/neural-sketch.dtx", "src/neural-sketch.ins" }
-installfiles = { "neural-sketch.sty", "neural-sketch-colors.sty" }
-docfiles = { "doc/latex/neural-sketch/neural-sketch-doc.tex" }
-
-checkengines = { "pdftex", "luatex", "xetex" }
+docfiledir = "doc"
+sourcefiledir = "src"
+typesetfiles = { "*.dtx", "*.tex" }
+packtdszip = true -- recommended for "tree" layouts
